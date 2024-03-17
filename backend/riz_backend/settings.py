2ce61fbb1fd180c0593ea539fc25b61260ac155a
arkoaikins <arkoaikins@gmail.com>
2024-03-17 12:05:08 -0400

    [feat]:configure settings.py to use custom User model from userauths app

--- backend/riz_backend/settings.py
+++ backend/riz_backend/settings.py
@@ -131,6 +131,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+AUTH_USER_MODEL = 'userauths.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
