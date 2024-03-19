bcc244ae37fcd305c5df21f9c8e6f375ea63bc4f
arkoaikins <arkoaikins@gmail.com>
2024-03-19 15:34:01 -0400

    [feat]:Add MyTokenObtainPairView for token pair generation using MyTokenPairSerializer

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from userauths.models import User, Profile
+from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
 
-# Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom view for obtaining token pairs(access and refresh token)
+    
+    This view extends the TokenObtainPairView provided by 
+    rest_framework_simplejwt to use my custom serializer,
+    MyTokenObtainPairSerialize, for generating the tokens
+    
+    The serializer_class attribure is set to MyTokenObtainPairSerializer
+    to override the default class used by TokenObtainPairView
+    """
+    serializer_class = MyTokenObtainPairSerializer
+
