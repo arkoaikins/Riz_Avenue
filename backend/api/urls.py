493fcf2f05e84e808180c934798f5c033b247fc3
arkoaikins <arkoaikins@gmail.com>
2024-03-19 18:29:14 -0400

    [feat]:Add a new URL pattern for user registration using the RegisterView to create a dedicated endpoint for user sign-up

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from userauths import views as userauths_views
 from store import views as store_views
 
+# Define URL patterns
 urlpatterns = [
-    path('user/token/', userauths_views.MyTokenObtainPairView.as_view())
+    # Define path for obtaining token pairs using MyTokenObtainPairView
+    path("user/token/", userauths_views.MyTokenObtainPairView.as_view()),
+    # Define a path for user registeration using RegisterView
+    path("user/register/", userauths_views.RegisterView.as_view()),
 ]
