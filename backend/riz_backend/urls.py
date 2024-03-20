2babe7389273c89f768cf60d1456a997ab3b2f53
arkoaikins <arkoaikins@gmail.com>
2024-03-20 09:56:20 -0400

    [feat]:Add OpenAPI schema view with Swagger UI integrations for API documentation

--- backend/riz_backend/urls.py
+++ backend/riz_backend/urls.py
@@ -14,15 +14,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-]
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Riz Avenue Backend APIs",
+        default_version="v1",
+        description=("This documentation provides an overview of Riz Avenue's "
+                     "backend APIs and their functionality"),
+        terms_of_service="https://github.com/arkoaikins/Riz_Avenue/tree/backend",  # nopep8
+        contact=openapi.Contact(email="arkoaikins@gmail.com"),
+        license=openapi.License(name="MIT license"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("api.urls")),
+    # Swagger  Documentation
+    path("", schema_view.with_ui(
+        "swagger", cache_timeout=0), name="schema-swagger-ui"),
+]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
