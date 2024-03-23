ca18736384d236899fc81001027572543b04ecfc
arkoaikins <arkoaikins@gmail.com>
2024-03-23 15:00:20 -0400

    [feat]:Add BASE_URL constant which holds the API endpoint for making request to server

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,10 @@
+/*
+The constand Base URL holds the base API endpoint for making requests
+to the server. It is currently set to the local development server
+running on port 8000 with API v1
+
+This constant help to centralize the base URL in a single location
+for easy management and updates
+*/
+
+export const BASE_URL = "http://127.0.0.1:8000/api/v1/"
