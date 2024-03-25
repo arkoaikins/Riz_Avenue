b5ceb67e55da55ccfc0d7938b2fbd9ac0eec950b
arkoaikins <arkoaikins@gmail.com>
2024-03-25 13:28:20 -0400

    [fix]:Add trailing slash to user/token/refresh URL path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # Define path for obtaining token pairs using MyTokenObtainPairView
     path("user/token/", userauths_views.MyTokenObtainPairView.as_view()),
     # Define path for refreshing access and refresh tokens using TokenRefreshView
-    path("user/token/refresh", TokenRefreshView.as_view()),
+    path("user/token/refresh/", TokenRefreshView.as_view()),
     # Define a path for user registeration using RegisterView
     path("user/register/", userauths_views.RegisterView.as_view()),
     
