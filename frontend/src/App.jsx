ed9197fc62f8e7337ec8d1c8e56131d48e5cb9ec
arkoaikins <arkoaikins@gmail.com>
2024-03-25 17:07:40 -0400

    [feat]:Set up routing for authentication components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import Login from './views/auth/Login'
+import Register from './views/auth/Register'
+import Dashboard from './views/auth/Dashboard'
+import Logout from './views/auth/Logout'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/logout' element={<Logout />} />
       
       </Routes>    
     
