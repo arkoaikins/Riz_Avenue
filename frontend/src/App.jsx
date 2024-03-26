88bdebdfda293b6049cd0101494ff3a40d23e1c0
arkoaikins <arkoaikins@gmail.com>
2024-03-26 19:57:10 -0400

    [feat]:add route for forgot password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './views/auth/Login'
 import Register from './views/auth/Register'
 import Dashboard from './views/auth/Dashboard'
 import Logout from './views/auth/Logout'
+import ForgotPassword from './views/auth/ForgotPassword'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<Dashboard />} />
         <Route path='/logout' element={<Logout />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
       
       </Routes>    
     
