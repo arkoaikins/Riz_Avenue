cc87f29f679fa84365efa86267f0f85ef6995b10
arkoaikins <arkoaikins@gmail.com>
2024-03-28 08:58:13 -0400

    [feat]:Update forgot password component with customized Bootstrap template

--- frontend/src/views/auth/ForgotPassword.jsx
+++ frontend/src/views/auth/ForgotPassword.jsx
@@ -1,42 +1,93 @@
 import { useState } from 'react'
 import creatAPIInstance from '../../utils/axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
 
 function ForgotPassword() {
     const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false);
 
     const navigate =  useNavigate()
     
 
     const handleReset = async () => {
+        setIsLoading(true);
         try {
             await creatAPIInstance.get(`user/password-reset/${email}/`).then((res) =>{
                 alert("An Email has been sent to you.")
+                setIsLoading(false);
             })
 
         } catch (error) {
             alert("NO account associated with this Email")
+            setIsLoading(false);
         }
                         
     }
 
     return (
-        <div>
-            <h1>ForgotPassword</h1>
-            <input 
-            onChange={(e) => setEmail(e.target.value)}
-            type="email" 
-            placeholder='Enter your Email' 
-            name=""
-            id="" 
-            />
-            <br />
-            <br />
-            <button onClick={handleReset}>Reset Password</button>
+      <>
+        <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+          <div className="container">
+            {/* Section: Login form */}
+            <section className="">
+              <div className="row d-flex justify-content-center">
+                <div className="col-xl-5 col-md-8">
+                  <div className="card rounded-5">
+                    <div className="card-body p-4">
+                      <h3 className="text-center">Riz Avenue Password Reset</h3>
+                      <br />
+                      <div>
+                        {/* Email input */}
+                        <div className="form-outline mb-4">
+                          <label className="form-label" htmlFor="loginName">
+                            Email
+                          </label>
+                          <input
+                            type="email"
+                            id="loginName"
+                            required
+                            name="email"
+                            value={email}
+                            className="form-control"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
 
+                        {isLoading === true ? (
+                          <button
+                            disabled
+                            className="btn btn-primary w-100"
+                            type="button"
+                          >
+                            <span className="mr-2">Sending Email... </span>
+                            <i className="fas fa-spinner fa-spin" />
+                          </button>
+                        ) : (
+                          <button
+                            onClick={handleReset}
+                            className="btn btn-primary w-100"
+                            type="button"
+                          >
+                            <span className="mr-2">Send Email </span>
+                            <i className="fas fa-paper-plane" />
+                          </button>
+                        )}
 
-        </div>
-    )
+                        <div className="text-center">
+                          <p className="mt-4">
+                            Login to Riz Avenue? <Link to="/login">Login</Link>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+      </>
+    );
 }
 
 export default ForgotPassword
