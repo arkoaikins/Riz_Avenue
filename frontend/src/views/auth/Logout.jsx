e4fb3102c90ca029ea2b4c949c456abf6dc02ce5
arkoaikins <arkoaikins@gmail.com>
2024-03-28 08:57:32 -0400

    [feat]:Update logout component with customized Bootstrap template

--- frontend/src/views/auth/Logout.jsx
+++ frontend/src/views/auth/Logout.jsx
@@ -7,12 +7,27 @@ function Logout() {
         logout()
     }, [])
     return (
-        <div>
-            <h1>Logout</h1>
-            <Link to={`/register`}>Register</Link> <br />
-            <Link to={`/login`}>Login</Link>
-        </div>
-    )
+      <div>
+        <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+          <div className="container">
+            {/* Section: Login form */}
+            <section className="">
+              <div className="row d-flex justify-content-center">
+                <div className="col-xl-5 col-md-8">
+                  <div className="card rounded-5">
+                    <div className="card-body p-4 text-center">
+                      <h4>Have a great day! Riz Avenue awaits your return.</h4>
+                      <Link to= {'/register'} className="btn btn-primary">Register <i className="fas fa-user-plus"></i></Link>
+                      <Link to= {'/login'} className="btn btn-primary ms-2">Login <i className="fas fa-sign-in"></i></Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
+    );
 }
 
 export default Logout
