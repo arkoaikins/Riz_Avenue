deae04f1e2e3bf7e5534be46464e8aebffad3aaf
arkoaikins <arkoaikins@gmail.com>
2024-03-28 08:55:49 -0400

    [feat]:Update Register component with customized Bootstrap template

--- frontend/src/views/auth/Register.jsx
+++ frontend/src/views/auth/Register.jsx
@@ -33,66 +33,146 @@ function Register() {
             alert(JSON.stringify(error))
         } else {
             navigate("/")
+            setIsLoading(false);
         }
     }
     return (
-        <div>
-            <div>Register</div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="text" 
-                placeholder='Full Name' 
-                name="" 
-                id=""
-                onChange={(e) => setFullname(e.target.value)}
-                />
-                <br />
-                <br />
-            
-                <input 
-                type="email" 
-                placeholder='Email' 
-                name="" 
-                id=""
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <br />
-                
-                <input 
-                type="number" 
-                placeholder='Phone' 
-                name="" 
-                id="" 
-                onChange={(e) => setMobile(e.target.value)}
-                />
-                <br />
-                <br />
-                
-                <input 
-                type="password" 
-                placeholder='Enter Password' 
-                name="" 
-                id=""
-                onChange={(e) => setPassword(e.target.value)}
-                />
-                <br />
-                <br />
-                
-                <input 
-                type="password" 
-                placeholder='Confirm Password' 
-                name="" 
-                id=""
-                onChange={(e) => setPassword2(e.target.value)}
-                />
-                <br />
-                <br />
-                <button type='submit'>Register</button>
+      <div>
+        <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+          <div className="container">
+            {/* Section: Login form */}
+            <section className="">
+              <div className="row d-flex justify-content-center">
+                <div className="col-xl-5 col-md-8">
+                  <div className="card rounded-5">
+                    <div className="card-body p-4">
+                      <h3 className="text-center">Join Riz Avenue</h3>
+                      <br />
 
-            </form>
-        </div>
-    )
+                      <div className="tab-content">
+                        <div
+                          className="tab-pane fade show active"
+                          id="pills-login"
+                          role="tabpanel"
+                          aria-labelledby="tab-login"
+                        >
+                          <form onSubmit={handleSubmit}>
+                            <div className="form-outline mb-4">
+                              <label className="form-label" htmlFor="Full Name">
+                                Full Name
+                              </label>
+                              <input
+                                type="text"
+                                id="username"
+                                placeholder="Full Name"
+                                required
+                                className="form-control"
+                                onChange={(e) => setFullname(e.target.value)}
+                              />
+                            </div>
+                            <div className="form-outline mb-4">
+                              <label className="form-label" htmlFor="loginName">
+                                Email
+                              </label>
+                              <input
+                                type="email"
+                                id="email"
+                                placeholder="Email Address"
+                                required
+                                className="form-control"
+                                onChange={(e) => setEmail(e.target.value)}
+                              />
+                            </div>
+
+                            <div className="form-outline mb-4">
+                              <label className="form-label" htmlFor="loginName">
+                                Mobile Number
+                              </label>
+                              <input
+                                type="number"
+                                id="phone"
+                                placeholder="Mobile Number"
+                                required
+                                className="form-control"
+                                onChange={(e) => setMobile(e.target.value)}
+                              />
+                            </div>
+                            <div className="form-outline mb-4">
+                              <label
+                                className="form-label"
+                                htmlFor="loginPassword"
+                              >
+                                Password
+                              </label>
+                              <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                className="form-control"
+                                onChange={(e) => setPassword(e.target.value)}
+                              />
+                            </div>
+                            {/* Password input */}
+                            <div className="form-outline mb-4">
+                              <label
+                                className="form-label"
+                                htmlFor="loginPassword"
+                              >
+                                Confirm Password
+                              </label>
+                              <input
+                                type="password"
+                                id="confirm-password"
+                                placeholder="Confirm Password"
+                                required
+                                className="form-control"
+                                onChange={(e) => setPassword2(e.target.value)}
+                              />
+                            </div>
+                            {/* Password Check */}
+                            {/* <p className='fw-bold text-danger'>
+                                                    {password2 !== password ? 'Passwords do not match' : ''}
+                                                </p> */}
+
+                            {isLoading === true ? (
+                              <button
+                                disabled
+                                className="btn btn-primary w-100"
+                                type="submit"
+                                //   disabled={"isLoading"}
+                              >
+                                <span className="mr-2">Creating account  </span>
+                                <i className="fas fa-spinner fa-spin" />
+                              </button>
+                            ) : (
+                              <button
+                                className="btn btn-primary w-100"
+                                type="submit"
+                                //   disabled={"isLoading"}
+                              >
+                                <span className="mr-2">Join Riz Avenue </span>
+                                <i className="fas fa-user-plus" />
+                              </button>
+                            )}
+
+                            <div className="text-center">
+                              <p className="mt-4">
+                                Already have an account?{" "}
+                                <Link to="/login">Login</Link>
+                              </p>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
+    );
 }
 
 export default Register
